e532f65b0a0a44f9828efb2447da6d13520bd008
Ben Scholom <bscholom@gmail.com>
2017-08-09 17:39:14 +0300

    alll get requests in place, category creation and deletion working, error management has not been touched yet

--- store.py
+++ store.py
@@ -8,7 +8,8 @@ connection = pymysql.connect(host='sql11.freesqldatabase.com',
                              password='4XXsiRzcZc',
                              db = 'sql11189256',
                              charset = 'utf8mb4',
-                             cursorclass = pymysql.cursors.DictCursor)
+                             cursorclass = pymysql.cursors.DictCursor,
+                             autocommit = True)
 
 @get("/admin")
 def admin_portal():
@@ -21,7 +22,19 @@ def products():
             sql = "SELECT * FROM products"
             cursor.execute(sql)
             result = cursor.fetchall()
-            return json.dumps({"STATUS": "SUCESS", "PRODUCTS": result, "CODE": 200})
+            print(json.dumps(result))
+            return json.dumps({"STATUS": "SUCCESS", "PRODUCTS": result, "CODE": 200})
+    except:
+        return json.dumps({'STATUS':"ERROR", "MSG": "Internal error", "CODE": 500})
+
+@get("/product/<id>")
+def get_prod(id):
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT FROM products WHERE id = {}".format(id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return json.dumps({'STATUS':"SUCCESS", "PRODUCT": result, "CODE": 200 })
     except:
         return json.dumps({'STATUS':"ERROR", "MSG": "Internal error", "CODE": 500})
 
@@ -30,10 +43,11 @@ def cat_prod_id(id):
     cat_id = id
     try:
         with connection.cursor() as cursor:
-            sql = "SELECT * FROM products where cat_id  = {}".format(cat_id)
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        return json.dumps({"STATUS": "SUCESS", "PRODUCTS": result, "CODE": 200})
+            sql = "SELECT * FROM products where category  = {}".format(cat_id)
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(json.dumps(result))
+            return json.dumps({"STATUS": "SUCESS", "PRODUCTS": result, "CODE": 200})
     except:
         return json.dumps({'STATUS': "ERROR", "MSG": "Internal error", "CODE": 500})
 
@@ -44,22 +58,58 @@ def cat_list():
             sql = "SELECT * FROM categories"
             cursor.execute(sql)
             result = cursor.fetchall()
-            return json.dumps({"STATUS": "SUCESS", "CATEGORIES": result, "CODE": 200})
+            print(result)
+            print(json.dumps(result))
+            return json.dumps({"STATUS": "SUCCESS", "CATEGORIES": result, "CODE": 200})
     except:
         return json.dumps({'STATUS':"ERROR", "MSG": "Internal error", "CODE": 500})
 
 @route("/category", method = "POST")
 def new_name():
     name = request.POST.get("name")
+    print(name)
     try:
         with connection.cursor() as cursor:
-            sql = "INSERT INTO categories (cat_id, cat_name) VALUES (0, {})".format(name)
+            sql = "INSERT INTO categories (name) VALUES ('{}')".format(name)
             cursor.execute(sql)
-            connection.commit()
-            result = cursor.lastrowid()
-            return json.dumps({"STATUS":"SUCCESS", "CAT_ID": result, "CODE": 200})
+            result = cursor.lastrowid
+            """sql2 = "SELECT id FROM categories WHERE name = ('{}')".format(name)
+            result = cursor.execute(sql2)
+            print(result)"""
+            return json.dumps({"STATUS":"SUCCESS", "CAT_ID": result, "CODE": 201})
     except:
-        return json.dumps({'STATUS':"ERROR", "MSG": "Does not work"})
+        return json.dumps({"STATUS":"ERROR", "MSG": 'something went wrong'})
+    """except Exception as e:
+        if str(e) == '200':
+            return json.dumps({'STATUS':"ERROR", "MSG": "category already exists"})
+        elif str(e) == '400':
+            return json.dumps({'STATUS': "ERROR", "MSG": "bad request"})
+        elif str(e) == '500':
+            return json.dumps({'STATUS': "ERROR", "MSG": "internal error"})"""
+
+@delete('/category/<id>')
+def del_cat(id):
+    cat_id = id
+    try:
+        with connection.cursor() as cursor:
+            sql2 = "DELETE FROM products WHERE Category = {}".format(cat_id)
+            cursor.execute(sql2)
+            sql = "DELETE FROM categories WHERE id = {}".format(cat_id)
+            cursor.execute(sql)
+            return json.dumps({"STATUS": "SUCCESS", "MSG": "category deleted successfully", "CODE": 201})
+    except:
+        return json.dumps({"STATUS": "ERROR", "MSG": 'something went wrong'})
+
+@post('/product')
+def add_edit_pro():
+    cat_id = request.POST.get('category')
+    description = request.POST.get('description')
+    price = request.POST.get('price')
+    title = request.POST.get('title')
+    favorite = request.POST.get('favorite')
+    img_url = request.POST.get('img_url')
+    id = request.POST.get('id')
+
 
 
 @get("/")
